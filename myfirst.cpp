3a9b9cd281cc95ecdcd4f51c903b13ec761b5e9b
yongchaoyongjie <215743260@qq.com>
2019-02-18 22:17:20 +0800

    c++的刚开始起步,学会用cin输入以及cout打印 然后懂得函数的有返回值和没有返回值关于c++的应用

---
+++ myfirst.cpp
@@ -0,0 +1,85 @@
+//#include <iostream>
+//#include <cmath>
+//void simon(int n);
+//int main(){
+//using namespace std;
+////用c++如何使用cout,也就是c中的printf
+//cout<<重载的时候用endl;是换行而用"endl"是字符串
+//cout<<"come up and c++ me some time .";
+//cout<< endl;
+//cout<<"you won not regret it !"<<endl;
+//
+//cout<<"the good,the";
+//cout <<"bad,";
+//cout<<"and the ukulele";
+//cout<<"endl";
+//cout <<endl;
+//cout<<"你好世界\n";
+//endl和"\n"都有换行作用,endl是控制字符,一般在用引号包起来用\n,其他都用endl比较好
+//c++变量定义
+//int carrots=25;
+//cout<<"i have";
+//cout<<carrots;
+//cout<<"carrots";
+//cout<<endl;
+//carrots=carrots-1;
+//cout<<"now i have"<<carrots<<"carrots"<<endl;
+//关于cin输入类似于scanf但是没有类型一说
+//int carrots;
+//cout<<"你有多少个胡萝卜"<<endl;
+//cin>>carrots;
+//cout<<"这里有两个胡萝卜";
+//carrots=carrots+2;
+//cout<<"现在你有"<<carrots<<"个胡萝卜"<<endl;
+//double area;
+//cout<<"enter the floor area,in square feet,of your home"<<endl;
+//
+//cin>>area;
+//double side=sqrt(area);
+//cout<<"that is equal of square"<<side<<"feet to the side "<<endl;
+//cout<<"how fascinating"<<endl;
+//无返回值的情况
+//simon(3);
+//cout<<"pick an intger";
+//int count;
+//cin>>count;
+//simon(count);
+//cout<<"done"<<endl;
+//  return 0;
+//}
+//void simon(int n){
+//using namespace std;
+//cout<<"simon says touch your hand"<<n<<"time"<<endl;
+//}
+//有返回值的函数调用
+#include <iostream>
+#include <cmath>
+int exchage(int);
+int main(){
+  //有返回值的函数调用计量之间的转换
+  using namespace std;
+// int stone;
+//cout<<"请输入重量单位为"<<"stone"<<endl;
+//cin>>stone;
+//int poud=exchage(stone);
+//cout<<stone<<"stone"<<"="<<poud<<"poud"<<endl;
+//  return 0;
+//}
+//int exchage(int sts){
+//  return 14*sts;
+//}
+//编写一个c++程序显示你的姓名和地址
+//cout<<"我的名字叫:\n陈永杰"<<endl;
+//cout<<"我的地址为:\n"<<"陕西省西安市XX大学"<<endl;
+//编写一个c++要求用户输入以long 为单位的距离,然后转换为码(long=200码)
+//cout<<"请输入长度\n";
+//int Long;
+//cin >>Long;
+//int ma=exchage(Long);
+//cout<<"输入结果为\n"<<Long<<"long="<<ma<<"ma"<<endl;
+//return 0;
+//}
+//int exchage(int std){
+//return 220*std;
+//}
+
